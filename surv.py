3b36bab9d31a909db5c61ce1b287e3a078419e5f
scampit <scampit@umich.edu>
2019-04-16 13:59:36 -0400

    Finished code for survival label determination from the Prognoscan database and integrating it with the current dataset

--- surv.py
+++ surv.py
@@ -1,45 +1,83 @@
 """
-New survival analysis
+Labeling survival data:
+
+To label the survival data, I will do the following:
+  * An HR > 2 and Cox p-value < 0.05 will be UPREG
+  * An HR < 0.5 and Cox p-value < 0.05 will be DOWNREG
+  * Everything else: NEUTRAL
+
 @author: Scott Campit
 """
-
+import os
 import pandas as pd
 import numpy as np
 
-# Filters
-remove_col = ["TYPE", "ID_DESCRIPTION", "DATA_POSTPROCESSING", "DATASET", "SUBTYPE", "ENDPOINT", "COHORT", "CONTRIBUTOR", "PROBE ID", "ARRAY TYPE", "N", "CUTPOINT", "MINIMUM P-VALUE", "CORRECTED P-VALUE", "ln(HR-high / HR-low)", "ln(HR)"]
-cancers = ["Breast cancer", "Ovarian cancer", "Colorectal cancer", "Lung cancer", "Prostate cancer", "Skin cancer", "Brain cancer", "Renal cell carcinoma", "Blood cancer"]
+def make_surv(input, cox, hr_up, hr_low):
+    """
+    make_surv will make a csv file containing the annotations by the Cox p-value and Hazard Ratio thresholds specified by the user. This file needs to be manually edited for multiple modes.
+    """
+    # Filters
+    remove_col = ["TYPE", "ID_DESCRIPTION", "DATA_POSTPROCESSING", "DATASET", "SUBTYPE", "ENDPOINT", "COHORT", "CONTRIBUTOR", "PROBE ID", "ARRAY TYPE", "N", "CUTPOINT", "MINIMUM P-VALUE", "CORRECTED P-VALUE", "ln(HR-high / HR-low)", "ln(HR)"]
+    cancers = ["Breast cancer", "Ovarian cancer", "Colorectal cancer", "Lung cancer", "Prostate cancer", "Skin cancer", "Brain cancer", "Renal cell carcinoma", "Blood cancer"]
 
-# Process data and only get the COX P-value and Hazard ratio
-df = pd.read_excel("./raw/prognoscan/prognoscan.xlsx")
-df = df.drop(columns=remove_col, axis=1)
-df["HR [95% CI-low CI-upp]"] = df["HR [95% CI-low CI-upp]"].str.replace('\[(.*?)\]', '', regex=True)
-df["CANCER TYPE"] = df["CANCER TYPE"].str.replace(' cancer', '', regex=True)
-df["CANCER TYPE"] = df["CANCER TYPE"].str.lower()+
-df = df[df["CANCER TYPE"].isin(cancers)]
-df["HR [95% CI-low CI-upp]"] = df["HR [95% CI-low CI-upp]"].apply(pd.to_numeric)
-df["SURV"] = ""
+    # Process data and only get the COX P-value and Hazard ratio
+    df = pd.read_excel(input)
+    df = df.drop(columns=remove_col, axis=1)
+    df["HR [95% CI-low CI-upp]"] = df["HR [95% CI-low CI-upp]"].str.replace('\[(.*?)\]', '', regex=True)
+    df = df[df["CANCER TYPE"].isin(cancers)]
+    df["HR [95% CI-low CI-upp]"] = df["HR [95% CI-low CI-upp]"].apply(pd.to_numeric)
+    df["SURV"] = ""
 
-# Make the actual labels
-df["SURV"].loc[df["HR [95% CI-low CI-upp]"] >= 2] = "UPREG"
-df["SURV"].loc[df["HR [95% CI-low CI-upp]"] <= 0.5] = "DOWNREG"
-df["SURV"].loc[(df["HR [95% CI-low CI-upp]"] < 2) & (df["HR [95% CI-low CI-upp]"] > 0.5)] = "NEUTRAL"
+    # Make the actual labels
+    df["SURV"].loc[(df["HR [95% CI-low CI-upp]"] >= hr_up) & (df["COX P-VALUE"] <= cox)] = "UPREG"
+    df["SURV"].loc[(df["HR [95% CI-low CI-upp]"] <= hr_low) & (df["COX P-VALUE"] <= cox)] = "DOWNREG"
+    df["SURV"].loc[(df["HR [95% CI-low CI-upp]"] >= hr_up) & (df["COX P-VALUE"] > cox)] = "NEUTRAL"
+    df["SURV"].loc[(df["HR [95% CI-low CI-upp]"] <= hr_low) & (df["COX P-VALUE"] > cox)] = "NEUTRAL"
+    df["SURV"].loc[(df["HR [95% CI-low CI-upp]"] < hr_up) & (df["HR [95% CI-low CI-upp]"] > hr_low)] = "NEUTRAL"
 
-# Majority vote on the labels if there are multiple genes and they each have different labels
-df = df.groupby(['ID_NAME', 'CANCER TYPE'])['SURV'].agg(pd.Series.mode).to_frame()
-df = df.reset_index()
+    # Majority vote on the labels if there are multiple genes and they each have different labels
+    df = df.groupby(['ID_NAME', 'CANCER TYPE'])['SURV'].agg(pd.Series.mode).to_frame()
+    df = df.reset_index()
 
-# I physically edited the csv file
-#df.to_csv('surv.csv')
+    # I physically edited the xlsx file. Need to devise conditional rule set to automatically determine labels for multiple modes
+    df.to_excel('surv.xlsx', index=False)
 
-df = pd.read_csv('surv.csv')
-print(df)
+#make_surv("./raw/prognoscan/prognoscan.xlsx", cox=0.05, hr_up=2, hr_low=0.05)
+
+canc_dict = {
+    'Breast cancer':'breast',
+    'Brain cancer':'cns',
+    'Colorectal cancer':'colon',
+    'Blood cancer':'leukemia',
+    'Skin cancer':'melanoma',
+    'Lung cancer':'nsclc',
+    'Ovarian cancer':'ovarian',
+    'Prostate cancer':'prostate',
+    'Renal cell carcinoma':'renal'
+}
+
+df = pd.read_excel('surv.xlsx')
+df = df.replace({'CANCER TYPE':canc_dict})
 
 # Read in the existing model and format it for our analysis
-fil = r"./data/new/breast.csv"
+fil = r"./data/original/breast.csv"
 model = pd.read_csv(fil)
+filname = os.path.basename(fil)
+canc, _ = os.path.splitext(filname)
+
+# Split gene name and cell line.
 model["Gene"], model["Cell Line"] = model["GENE"].str.split('_', 1).str
 model = model.drop(columns='GENE', axis=1)
 
-# Drop existing labels
+# Drop existing survival labels
 model = model.drop(columns="SURV", axis=1)
+
+# Create new survival label as empty and populate later
+tmp = df[df["CANCER TYPE"] == canc]
+tmp = tmp.drop(columns='CANCER TYPE')
+
+model = pd.merge(model, tmp, how='left', left_on='Gene', right_on='ID_NAME').drop(columns='ID_NAME')
+model = model.fillna('NEUTRAL')
+
+model = model.set_index(['Gene', 'Cell Line'])
+model = model.reset_index()
