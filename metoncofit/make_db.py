487c595ca77ede5061f2015cdef9adc02912bfab
ScottCampit <scampit@umich.edu>
2019-07-01 17:23:26 -0400

    Need to work on df pivots

--- metoncofit/make_db.py
+++ metoncofit/make_db.py
@@ -5,12 +5,7 @@ db.py creates the metoncofit dataframe that can be used for several web and deve
 @author: Scott Campit
 """
 
-import sys
-import copy
-import operator
-import argparse
-import re
-import os
+import sys, copy, operator, argparse, re, os
 
 import numpy as np
 import pandas as pd
@@ -29,10 +24,10 @@ all_dfs = []
 targ = ["TCGA_annot", "CNV", "SURV"]
 var_excl = ["TCGA gene expression fold change", "CNV gain/loss ratio"]
 
-x = 0
+x=0
 for fil in os.listdir('./../data/median/'):
     # Iterate between models
-    x = x+1
+    x=x+1
     for t in targ:
 
         # Proprocessing data
@@ -45,35 +40,32 @@ for fil in os.listdir('./../data/median/'):
         if datapath is None:
             datapath = './../data/original/'
 
-        canc = fil.replace(".csv", "")
+        canc = fil.replace(".csv","")
         canc_dict = {
-            'breast': 'Breast Cancer',
-            'cns': 'Glioma',
-            'colon': 'Colorectal Cancer',
-            'complex': 'Pan Cancer',
-            'leukemia': 'B-cell lymphoma',
-            'melanoma': 'Melanoma',
-            'nsclc': 'Lung Cancer',
-            'ovarian': 'Ovarian Cancer',
-            'prostate': 'Prostate Cancer',
-            'renal': 'Renal Cancer'
-            }
+            'breast':'Breast Cancer',
+            'cns':'Glioma',
+            'colon':'Colorectal Cancer',
+            'complex':'Pan Cancer',
+            'leukemia':'B-cell lymphoma',
+            'melanoma':'Melanoma',
+            'nsclc':'Lung Cancer',
+            'ovarian':'Ovarian Cancer',
+            'prostate':'Prostate Cancer',
+            'renal':'Renal Cancer'
+        }
         canc = canc_dict.get(canc)
 
-        df_names = pd.read_csv(
-            "./../labels/real_headers.txt", sep='\t', names=['Original', 'New'])
-        names = dict([(i, nam)
-                      for i, nam in zip(df_names['Original'], df_names['New'])])
+        df_names = pd.read_csv("./../labels/real_headers.txt", sep='\t', names=['Original', 'New'])
+        names = dict([(i, nam) for i, nam in zip(df_names['Original'], df_names['New'])])
         df = pd.read_csv(datapath+fil, index_col=None)
-        df = df.drop(columns=['TCGA_val', 'CNV_val'], axis=1)
+        df = df.drop(columns=['TCGA_val','CNV_val'],axis=1)
         df = df.rename(columns=names)
-        df = df.set_index(['Genes', 'Cell Line'])
+        df = df.set_index(['Genes','Cell Line'])
 
         # We are label encoding the subsystem and datapath labels
         le = preprocessing.LabelEncoder()
         df["RECON1 subsystem"] = le.fit_transform(df["RECON1 subsystem"])
-        df["Metabolic subnetwork"] = le.fit_transform(
-            df["Metabolic subnetwork"])
+        df["Metabolic subnetwork"] = le.fit_transform(df["Metabolic subnetwork"])
 
         excl_targ = {'TCGA annotation', 'SURV', 'CNV'}
         tmp = excl_targ.remove(t)
@@ -81,14 +73,13 @@ for fil in os.listdir('./../data/median/'):
         df = df.drop(columns=excl_targ)
         classes = df[t]
         header = df.columns
-        df1 = df.copy(deep=True)  # contains target classes
-        df = df.drop(columns=t)  # doesn't contain target classes
+        df1 = df.copy(deep=True) # contains target classes
+        df = df.drop(columns=t) # doesn't contain target classes
 
         data = np.array(df).astype(np.float)
         data = RobustScaler().fit_transform(data)
 
-        new_data, orig_data, new_classes, orig_classes = train_test_split(
-            data, classes, test_size=0.3)
+        new_data, orig_data, new_classes, orig_classes = train_test_split(data, classes, test_size=0.3)
 
         ros = RandomOverSampler()
         data, classes = ros.fit_sample(new_data, new_classes)
@@ -98,8 +89,7 @@ for fil in os.listdir('./../data/median/'):
         while(feat < data.shape[1]-1):
             trees = 5
             while(trees <= 500):
-                rfc = RandomForestClassifier(
-                    n_estimators=trees, max_features=feat)
+                rfc = RandomForestClassifier(n_estimators=trees, max_features=feat)
                 rfc.fit(data, classes)
                 trees = trees + 1500
             feat = feat + 20
@@ -107,15 +97,14 @@ for fil in os.listdir('./../data/median/'):
             mean_acc = rfc.score(orig_data, orig_classes)
 
         if(t == "CNV"):
-            targ_labels = ["GAIN", "NEUT", "LOSS"]
+            targ_labels = ["GAIN","NEUT","LOSS"]
             targ_dict = {'NEUT': 0, 'LOSS': 0, 'GAIN': 0}
         else:
-            targ_labels = ["UPREG", "NEUTRAL", "DOWNREG"]
+            targ_labels = ["UPREG","NEUTRAL","DOWNREG"]
             targ_dict = {'NEUTRAL': 0, 'DOWNREG': 0, 'UPREG': 0}
 
         df1 = df1.reset_index()
-        one_gene_df = df1.drop(columns="Cell Line").groupby(
-            ["Genes", t]).median().reset_index().set_index("Genes")
+        one_gene_df = df1.drop(columns="Cell Line").groupby(["Genes", t]).median().reset_index().set_index("Genes")
         one_gene_class = pd.DataFrame(one_gene_df[t])
         one_gene_class = one_gene_class.reset_index()
 
@@ -139,7 +128,7 @@ for fil in os.listdir('./../data/median/'):
             v2 = neut_df[col].median()
             v3 = down_df[col].median()
 
-            correl = np.corrcoef([v1, v2, v3], [1.0, 0.0, -1.0])
+            correl = np.corrcoef([v1,v2,v3],[1.0,0.0,-1.0])
 
             if(np.isnan(correl[0][1]) != True):
                 column_squigly[col] = correl[0][1]
@@ -153,8 +142,7 @@ for fil in os.listdir('./../data/median/'):
             temp_dict_feat = {}
             for i, j in zip(header, to_be_mapped):
                 temp_dict_feat[i] = j
-            sorted_d = sorted(temp_dict_feat.items(),
-                              key=operator.itemgetter(1), reverse=True)
+            sorted_d = sorted(temp_dict_feat.items(), key=operator.itemgetter(1), reverse=True)
             return sorted_d
         sorted_d = idx_change(header, rfc.feature_importances_)
 
@@ -162,15 +150,15 @@ for fil in os.listdir('./../data/median/'):
         gini = []
         corr = []
 
-        x = 0
-        while(x < 130):  # Get the first 10 features
+        x=0
+        while(x<136): # Get the first 10 features
             tempa = sorted_d[x]
             feat.append(tempa[0])
             gini.append(tempa[1])
-            corr.append((column_squigly[tempa[0]]))
+            corr.append(str(column_squigly[tempa[0]]))
             x = x+1
 
-        importance = pd.DataFrame({"Feature": feat, "Gini": gini, "R": corr})
+        importance = pd.DataFrame({"Feature":feat, "Gini":gini, "R":corr})
 
         # Map to label
         if t == 'CNV':
@@ -193,25 +181,22 @@ for fil in os.listdir('./../data/median/'):
         features = list(importance['Feature'])
 
         up = tmparr_up[features].T
-        up = up.merge(importance, how='inner',
-                      left_index=True, right_on='Feature')
-        up = pd.melt(up, id_vars=["Feature", "Gini", "R"],
-                     var_name="Gene", value_name="Value")
+        up = up.merge(importance, how='inner', left_index=True, right_on='Feature')
+        up = pd.melt(up, id_vars=["Feature", "Gini", "R"], var_name="Gene", value_name="Value")
         up["Type"] = class_col[0]
+        up = up.sort_values(['Gini'], ascending=False)
 
         neut = tmparr_neut[features].T
-        neut = neut.merge(importance, how='inner',
-                          left_index=True, right_on='Feature')
-        neut = pd.melt(
-            neut, id_vars=["Feature", "Gini", "R"], var_name="Gene", value_name="Value")
+        neut = neut.merge(importance, how='inner', left_index=True, right_on='Feature')
+        neut = pd.melt(neut, id_vars=["Feature", "Gini", "R"], var_name="Gene", value_name="Value")
         neut["Type"] = class_col[1]
+        neut = neut.sort_values(['Gini'], ascending=False)
 
         down = tmparr_down[features].T
-        down = down.merge(importance, how='inner',
-                          left_index=True, right_on='Feature')
-        down = pd.melt(
-            down, id_vars=["Feature", "Gini", "R"], var_name="Gene", value_name="Value")
+        down = down.merge(importance, how='inner', left_index=True, right_on='Feature')
+        down = pd.melt(down, id_vars=["Feature", "Gini", "R"], var_name="Gene", value_name="Value")
         down["Type"] = class_col[2]
+        down = down.sort_values(['Gini'], ascending=False)
 
         final_df = pd.concat([up, neut, down], axis=0)
         final_df['Cancer'] = canc
@@ -226,33 +211,40 @@ for fil in os.listdir('./../data/median/'):
 
         final_df["Target"] = t
         final_df = final_df[~final_df['Cancer'].str.contains('Pan')]
+        final_df = final_df.sort_values(['Gini'], ascending=False)
+        final_df = final_df.set_index('Feature')
+        tmp = final_df.copy(deep=True)
+        idx = tmp.index.unique()
 
-        # Improve data storage efficiency
-        final_df = final_df.round(2)
-        final_df['Gini'] = pd.to_numeric(final_df['Gini'], downcast='float')
-        final_df['R'] = pd.to_numeric(final_df['R'], downcast='float')
-        final_df['Value'] = pd.to_numeric(final_df['Value'], downcast='float')
-        final_df['Type'] = final_df['Type'].astype('category')
-        final_df['Cancer'] = final_df['Cancer'].astype('category')
-        final_df['Target'] = final_df['Target'].astype('category')
-        final_df['Feature'] = final_df['Feature'].astype('category')
-        final_df['Gene'] = final_df['Gene'].astype('category')
-        print(canc)
-        
-        from openpyxl import load_workbook
-        book = load_workbook('./../output/Tables/SI.xlsx')
-        writer = pd.ExcelWriter(
-            './../output/Tables/SI.xlsx', engine='openpyxl')
-        writer.book = book
-        writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+        # New Iteration
+        #final_df = final_df.groupby(['Feature', 'Target', 'Type', 'Cancer']).sum()
+        #final_df = final_df.sort_values(['Gini'], ascending=False)
+        #final_df = final_df.drop(['Gini'], axis=1)
+        #final_df = final_df.T
 
-        if ("S. Table "+str(x+9)+" | "+canc) in book:
-            final_df.to_excel(writer, sheet_name=("S. Table "+str(x+9)+" | "+canc),
-                              startrow=writer.sheets[("S. Table "+str(x+9)+" | "+canc)].max_row, header=False)
-        else:
-            final_df.to_excel(writer, sheet_name=(
-                "S. Table "+str(x+9)+" | "+canc), header=True)
-        writer.save()
+        final_df = pd.pivot_table(final_df, index=['Gene', 'Type', 'Target', 'Cancer'], columns= final_df.index, aggfunc=np.sum)
+        #final_df['Type'] = final_df['Type'].astype('category')
+        #final_df['Cancer'] = final_df['Cancer'].astype('category')
+        #final_df['Target'] = final_df['Target'].astype('category')
+        #final_df['Feature'] = final_df['Feature'].astype('category')
+
+        final_df = final_df.round(3)
+        print(final_df.columns)
+        final_df = final_df[[idx]]
+        print(final_df)
+
+        #from openpyxl import load_workbook
+        #book = load_workbook('./../output/Tables/SI.xlsx')
+        #writer = pd.ExcelWriter('./../output/Tables/SI.xlsx', engine='openpyxl')
+        #writer.book = book
+        #writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+        #if ("S. Table | "+canc) in book:
+    #        final_df.to_excel(writer, sheet_name=("S. Table | "+canc), startrow=writer.sheets[("S. Table | "+canc)].max_row, header=False)
+        #else:
+    #        final_df.to_excel(writer, sheet_name=("S. Table | "+canc), header=True)
+    #    writer.save()
+        #all_dfs.append(final_df)
 
 #big_df = pd.concat(all_dfs, axis=0, ignore_index=True)
 #big_df.to_csv("db.csv")
