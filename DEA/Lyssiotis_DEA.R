9be0ec79d96294009d89589bd2b77e3df8bdb3d1
jennadiegel <49460705+jennadiegel@users.noreply.github.com>
2020-05-15 10:05:11 -0400

    Update Lyssiotis.R
    exported all data and statistically significant genes

--- DEA/Lyssiotis_DEA.R
+++ DEA/Lyssiotis_DEA.R
@@ -19,6 +19,7 @@ um90 <- import("~jennadiegel/Documents/SURE/DES/CCLs_UM90.csv")
 
 cells <- list(bx, cp, pn, tu, tut, um2, um90)
 titles <- list("Log2 distribution of BxPC3","Log2 distribution of Capan1","Log2 distribution of PANC1","Log2 distribution of TU8902","Log2 distribution of TU8988T","Log2 distribution of UM2","Log2 distribution of UM90")
+cell_names <- list("BxPC3","Capan1","PANC1","TU8902","TU8988T","UM2","UM90")
 
 #initiate lists to hold fold changes and gene names outside the loop
 all_data <- list()
@@ -27,6 +28,7 @@ gene_names_list <- list()
 inf_gene_names <- list()
 z_scores <- list()
 p_values <- list()
+all_map_genes <- list()
 count <- 1
 
 for (cell in cells) {
@@ -47,38 +49,50 @@ for (cell in cells) {
   #transform fold change to get normal distribution using log2
   cell_fold_change_log2 <- log2(cell_fold_change)
   
-  #find NaN and inf values and keep only the non-NaN and non-inf values
-  bool <- (is.nan(cell_fold_change_log2)|is.infinite(cell_fold_change_log2))
-  cell_fold_change_keep <- cell_fold_change[!bool]
-  cell_fold_change_log2_keep <- cell_fold_change_log2[!bool]
-  gene_names_keep <- gene_names[!bool]
-  
   #separate out inf values 
   bool_inf <- is.infinite(cell_fold_change_log2)
   gene_names_inf <- gene_names[bool_inf]
   
+  #find NaN and inf values and keep only the non-NaN and non-inf values
+  bool <- (is.nan(cell_fold_change_log2)|is.infinite(cell_fold_change_log2))
+  cell_fold_change_keep <- cell_fold_change[!bool]
+  cell_fold_change_log2_keep <- cell_fold_change_log2[!bool]
+  gene_names_keep <- c(gene_names[!bool], gene_names_inf)
+  
+  cell_fold_change_ <- c(cell_fold_change_keep, rep(max(cell_fold_change_keep),length(gene_names_inf)))
+  cell_fold_change_log2_ <- c(cell_fold_change_log2_keep, rep(max(cell_fold_change_log2_keep),length(gene_names_inf)))
+  
   #calculate z score and p value for non inf values
   z__score <- scale(cell_fold_change_log2_keep)
+  z__score <- c(z__score, rep(max(z__score), length(gene_names_inf)))
   z_scores[[count]] <- z__score
   
   p__value <- 2*pnorm(-abs(z__score))
-  p_values[[count]] <- p__value
+  p_values[[count]] <- c(p__value, rep(10^(-50),length(gene_names_inf)))
   
   data_storage <- data.frame(
     gene_name <- gene_names_keep,
-    fold_change <- cell_fold_change_keep,
-    log2_fold_change <- cell_fold_change_log2_keep,
-    z_score <- z__score,
+    fold_change <- cell_fold_change_,
+    log2_fold_change <- cell_fold_change_log2_,
+    z_score <- z_scores[[count]],
     p_value <- p__value)
   
+  gene_data <- data.frame(
+    map_genes <- map_genes
+  )
+  
+  bool_p <- (p__value <= 0.05)
+  map_genes <- gene_name[bool_p]
   all_data[[count]] <- data_storage
   
   #all_fold_changes_log2[[count]] <- cell_fold_change_keep #redundant line to work with data outside loop
   gene_names_list[[count]] <- gene_names_keep
   inf_gene_names[[count]] <- gene_names_inf
+  all_map_genes[[count]] <- map_genes
   hist(cell_fold_change_log2_keep, probability = T, breaks = 50,
     main = titles[[count]])
     
   count <- count + 1}
 
-
+export(all_data, 'lyossiotis.xlsx')
+export(all_map_genes, 'map_genes.xlsx')
