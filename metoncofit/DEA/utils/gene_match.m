9a7dd410ee87e4225e9e9773fba27b7407dec4cc
jennadiegel <49460705+jennadiegel@users.noreply.github.com>
2020-05-18 15:45:03 -0400

    Improved naming of variables

--- metoncofit/DEA/utils/gene_match.m
+++ metoncofit/DEA/utils/gene_match.m
@@ -1,7 +1,7 @@
 function [ data_table ] = gene_match( model_genes, cell_line_genes, fold_change_data )
 
 %combine the contents of multiple cells into one string
-gene_names = join(model_genes);
+gene = join(model_genes);
 
 %initialize an empty cell array to input the matches into
 matching_genes = cell(1,length(cell_line_genes));
@@ -9,7 +9,7 @@ for a = 1:length(cell_line_genes)
     
     reg = strcat('\s',cell_line_genes(a),'\s');
     %find matches and for genes listed more than once, take only one match
-    matching_genes(1,a) = regexp(gene_names,reg,'match','once');
+    matching_genes(1,a) = regexp(gene,reg,'match','once');
     
 end
 
