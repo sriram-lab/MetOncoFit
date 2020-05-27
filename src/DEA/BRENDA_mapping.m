278e7ffe26e0025e3bdea4988a241a0f7bc0648f
jennadiegel <jdiegel@umich.edu>
2020-05-27 14:29:37 -0400

    added comments for clarity

--- src/DEA/BRENDA_mapping.m
+++ src/DEA/BRENDA_mapping.m
@@ -2,6 +2,7 @@
 [~,km_ec] = xlsread('km.xlsx','A2:A20886');
 km = xlsread('km.xlsx','B:B');
 
+%want only positive kinetics parameter values
 km_ec = km_ec(km>0);
 km = km(km>0);
 
@@ -9,8 +10,10 @@ km = km(km>0);
 [km_ec,i] = sort(km_ec);
 km = km(i);
 
+% join into one string to search through
 ec_joined = join(km_ec);
 
+%get list of unique ec numbers
 unique_ec = unique(km_ec);
 
 %initializing empty cells and matricies to hold data
@@ -30,6 +33,7 @@ end
 index1 = 1;
 index2 = 0;
     
+%average km value if there are multiple entries
 km_averaged = zeros(length(unique_ec),1);
 for b = 1:length(unique_ec)
 
@@ -40,6 +44,7 @@ for b = 1:length(unique_ec)
     index1 = index2 + 1;
 end
 
+%store data in a cell array
 km_averaged = num2cell(km_averaged);
 ecs_kms = [unique_ec, km_averaged];
 
@@ -48,6 +53,7 @@ ecs_kms = [unique_ec, km_averaged];
 [~,kcat_ec] = xlsread('kcat.xlsx','G2:G10381');
 kcat = xlsread('kcat.xlsx','F:F');
 
+%keep only postive values
 kcat_ec = kcat_ec(kcat>0);
 kcat = kcat(kcat>0);
 
@@ -55,8 +61,10 @@ kcat = kcat(kcat>0);
 [kcat_ec,i] = sort(kcat_ec);
 kcat = kcat(i);
 
+% join into one string to search through
 ec_joined = join(kcat_ec);
 
+% get list of unique ec numbers
 unique_ec = unique(kcat_ec);
 
 %initializing empty cells and matricies to hold data
@@ -92,21 +100,22 @@ ecs_kcats = [unique_ec, kcat_averaged];
 
 %% ec number to gene symbol
 
+%variables of conversion data are labeled with _c for clarity
 names = readcell('geneNamesAndSymbols.xlsx');
-ecs = names(:,3);
-symbols = names(:,1);
+ecs_c = names(:,3);
+symbols_c = names(:,1);
 
 %remove <missing> from ecs 
-symbols = symbols(cellfun(@ischar,ecs));
-ecs = ecs(cellfun(@ischar,ecs));
+symbols_c = symbols_c(cellfun(@ischar,ecs_c));
+ecs_c = ecs_c(cellfun(@ischar,ecs_c));
 
-%remove commas 
+%remove commas from string
 reg = ',';
 
-for a = 1:length(ecs)
+for a = 1:length(ecs_c)
     
-    index = regexp(ecs{a}, reg);
-    ecs{a}(index) = ' ';
+    index = regexp(ecs_c{a}, reg);
+    ecs_c{a}(index) = ' ';
     
 end
 
@@ -116,21 +125,25 @@ km_ec = join(km_ec);
 [~,kcat_ec] = xlsread('kcat.xlsx','G2:G10381');
 kcat_ec = join(kcat_ec);
 
-% example reg = '(1\.1\.1\.1 |1\.1\.1\.1$| 1\.1\.1\.1$)';
+% example reg = '(1\.1\.1\.1 |1\.1\.1\.1$| 1\.1\.1\.1$)' 
+%to account for being the first entry, last entry, or only entry
+%because some gene symbols map to multiple ec numbers
 
 %find all . and add a \ before them
+%so the regular expression will not use . as any character
 reg = '\.';
-for c = 1:length(ecs)
+for c = 1:length(ecs_c)
     
-    ecs{c} = regexprep(ecs{c},reg,'\\.');
+    ecs_c{c} = regexprep(ecs_c{c},reg,'\\.');
     
 end
 
-km_ecs = cell(1, length(ecs));
-kcat_ecs = cell(1, length(ecs));
-for b = 1:length(ecs)
+km_ecs = cell(1, length(ecs_c));
+kcat_ecs = cell(1, length(ecs_c));
+for b = 1:length(ecs_c)
     
-    reg = strcat(ecs{b}, {' |'},ecs{b}, {'$| '}, ecs{b}, '$');
+    %make expression as explained in comment above
+    reg = strcat(ecs_c{b}, {' |'},ecs_c{b}, {'$| '}, ecs_c{b}, '$');
     km_ecs(1,b) = regexp(km_ec, reg, 'match','once');
     kcat_ecs(1,b) = regexp(kcat_ec, reg, 'match','once');
     
@@ -138,11 +151,11 @@ end
 
 % some ec numbers map to multiple symbols, I kept all possible symbols to
 % increase the chance of mapping to a gene symbol in the model
-km_symbols = symbols(~cellfun('isempty',km_ecs));
+km_symbols = symbols_c(~cellfun('isempty',km_ecs));
 km_ecs = km_ecs(~cellfun('isempty',km_ecs))';
 km_sym_ec = [km_symbols, km_ecs, cell(length(km_ecs),1)];
 
-kcat_symbols = symbols(~cellfun('isempty',kcat_ecs));
+kcat_symbols = symbols_c(~cellfun('isempty',kcat_ecs));
 kcat_ecs = kcat_ecs(~cellfun('isempty',kcat_ecs))';
 kcat_sym_ec = [kcat_symbols, kcat_ecs, cell(length(kcat_ecs),1)];
 
--- src/DEA/sabioRK_mapping.m
+++ src/DEA/sabioRK_mapping.m
@@ -1,45 +1,56 @@
 %% loading data and filtering out unwanted values/missing data
+%for legibility, data from the sabio data file are labeled with _s
 sabio_data = readcell('homosapien.xlsx');
-param_type = sabio_data(2:end,10);
-uniprotIDs = sabio_data(2:end,8);
-param_val = sabio_data(2:end,12);
-enzyme_type = sabio_data(2:end,7);
+param_type_s = sabio_data(2:end,10);
+uniprotIDs_s = sabio_data(2:end,8);
+param_val_s = sabio_data(2:end,12);
+enzyme_type_s = sabio_data(2:end,7);
 
 %boolean vectors to find the data that is wanted (done in multiple lines to
 %improve legibility
-has_id = cellfun(@ischar,uniprotIDs);
-is_wt = contains(enzyme_type, 'wildtype');
-is_kcat = strcmp('kcat',param_type);
-is_km = strcmp('Km',param_type);
-is_vmax = strcmp('Vmax',param_type);
+has_id = cellfun(@ischar,uniprotIDs_s);
+is_wt = contains(enzyme_type_s, 'wildtype');
+is_kcat = strcmp('kcat',param_type_s);
+is_km = strcmp('Km',param_type_s);
+is_vmax = strcmp('Vmax',param_type_s);
 
-%combining data that is desired
+%combining data that is desired, must have id and be wt and be either a
+%kcat, km, or vmax parameter type
 keep = has_id & is_wt & (is_kcat | is_km | is_vmax);
 
 %remove parameters that are not wanted
-param_type = param_type(keep);
-uniprotIDs = uniprotIDs(keep);
-param_val = param_val(keep);
-enzyme_type = enzyme_type(keep);
+param_type_s = param_type_s(keep);
+uniprotIDs_s = uniprotIDs_s(keep);
+param_val_s = param_val_s(keep);
+enzyme_type_s = enzyme_type_s(keep);
 
 
 %% 
 %import list of uniprotIDs and gene symbols
+
+%IDs from the ID_list and symbols from the symbol_list correspond
+
+%for legibility variables that are from the uniprot to symbol conversion
+%file are labeled with _c
 uniprotID_conv = readcell('uniprotToSymbol.xlsx');
-ID_list = uniprotID_conv(:,2);
-symbol_list = uniprotID_conv(:,1);
+ID_list_c = uniprotID_conv(:,2);
+symbol_list_c = uniprotID_conv(:,1);
 
 %remove symbols that dont have an ID
-symbol_list = symbol_list(cellfun(@ischar,ID_list));
-ID_list = ID_list(cellfun(@ischar,ID_list));
+symbol_list_c = symbol_list_c(cellfun(@ischar,ID_list_c));
+ID_list_c = ID_list_c(cellfun(@ischar,ID_list_c));
 
 %find matches and store locations of matches 
 %preset locations matrix to arbitrarily large size
+%first row is the location within the sabio data 
+%second row is the location within the conversion data
 locations = zeros(2,50000);
 count = 1;
-for a = 1:length(uniprotIDs)
-    for b = 1:length(ID_list)
-        match = regexp(ID_list{b}, uniprotIDs{a}, 'match');
+for a = 1:length(uniprotIDs_s)
+    for b = 1:length(ID_list_c)
+        match = regexp(ID_list_c{b}, uniprotIDs_s{a}, 'match');
+        
+        %store a list of locations to match symbols to parameter values
         if ~isempty(match)
             locations(1, count) = a;
             locations(2, count) = b;
@@ -52,10 +63,13 @@ end
 
 %first column = location within uniprotID, second column = location within
 %ID_list
+%remove empty/zero entries from large locations matrix
 locs = [nonzeros(locations(1,:)), nonzeros(locations(2,:))];
 
 %% organize data by parameter type
 % data = col 1 = symbol col 2 = param type col 3 = param value
+
+%using the locations to match symbol to parameter type and parameter value
 km_data = cell(length(locs),3,3);
 km_count = 1;
 kcat_data = cell(length(locs),3);
@@ -64,22 +78,22 @@ vmax_data = cell(length(locs),3);
 vmax_count = 1;
 for c = 1:length(locs)
     
-    if isequal(param_type{locs(c,1)},'Km')
-        km_data{km_count, 1} = symbol_list{locs(c,2)};
-        km_data{km_count, 2} = param_type{locs(c,1)};
-        km_data{km_count, 3} = param_val{locs(c,1)};
+    if isequal(param_type_s{locs(c,1)},'Km')
+        km_data{km_count, 1} = symbol_list_c{locs(c,2)};
+        km_data{km_count, 2} = param_type_s{locs(c,1)};
+        km_data{km_count, 3} = param_val_s{locs(c,1)};
         km_count = km_count + 1;
         
-    elseif isequal(param_type{locs(c,1)},'kcat')
-        kcat_data{kcat_count, 1} = symbol_list{locs(c,2)};
-        kcat_data{kcat_count, 2} = param_type{locs(c,1)};
-        kcat_data{kcat_count, 3} = param_val{locs(c,1)};
+    elseif isequal(param_type_s{locs(c,1)},'kcat')
+        kcat_data{kcat_count, 1} = symbol_list_c{locs(c,2)};
+        kcat_data{kcat_count, 2} = param_type_s{locs(c,1)};
+        kcat_data{kcat_count, 3} = param_val_s{locs(c,1)};
         kcat_count = kcat_count + 1;
         
     else
-        vmax_data{vmax_count, 1} = symbol_list{locs(c,2)};
-        vmax_data{vmax_count, 2} = param_type{locs(c,1)};
-        vmax_data{vmax_count, 3} = param_val{locs(c,1)};
+        vmax_data{vmax_count, 1} = symbol_list_c{locs(c,2)};
+        vmax_data{vmax_count, 2} = param_type_s{locs(c,1)};
+        vmax_data{vmax_count, 3} = param_val_s{locs(c,1)};
         vmax_count = vmax_count + 1;
         
     end
